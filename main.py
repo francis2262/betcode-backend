2fcd6da99a501ba2b25d53d8d15d7446fbd6bbbd
francis2262 <agufrancis567@gmail.com>
2025-08-28 04:54:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message": "Backend is running successfully 🚀"}
+def root():
+    return {"message": "Welcome to Betcode Backend!"}
